81ec5fb6c652dfff1e913425d8643662e56fa6a8
Goodbird <goodbirdmail@yandex.ru>
2024-01-28 09:49:51 +0300

    Added model and animation selection GUIs

---
+++ src/main/java/com/goodbird/cnpcgeckoaddon/client/gui/GuiModelAnimation.java
@@ -0,0 +1,111 @@
+package com.goodbird.cnpcgeckoaddon.client.gui;
+
+import com.goodbird.cnpcgeckoaddon.data.CustomModelData;
+import com.goodbird.cnpcgeckoaddon.mixin.IDataDisplay;
+import net.minecraft.util.ResourceLocation;
+import noppes.npcs.client.gui.util.GuiNPCInterface;
+import noppes.npcs.entity.EntityNPCInterface;
+import noppes.npcs.shared.client.gui.components.GuiButtonNop;
+import noppes.npcs.shared.client.gui.components.GuiLabel;
+import noppes.npcs.shared.client.gui.components.GuiTextFieldNop;
+import noppes.npcs.shared.client.gui.listeners.ITextfieldListener;
+import com.goodbird.cnpcgeckoaddon.utils.AnimationFileUtil;
+import software.bernie.geckolib3.resource.GeckoLibCache;
+
+public class GuiModelAnimation extends GuiNPCInterface implements ITextfieldListener {
+
+    @Override
+    public void init() {
+        super.init();
+        int y = guiTop + 44;
+        addSelectionBlock(1,y,"Animation File:",getModelData(npc).getAnimFile());
+        addSelectionBlock(2,y+=23,"Idle:",getModelData(npc).getIdleAnim());
+        addSelectionBlock(3,y+=23,"Walk:",getModelData(npc).getWalkAnim());
+        addSelectionBlock(4,y+=23,"Attack:",getModelData(npc).getAttackAnim());
+        addSelectionBlock(5,y+23,"Hurt:",getModelData(npc).getHurtAnim());
+        this.addButton(new GuiButtonNop(this, 670, width - 22, 2, 20, 20, "X"));
+    }
+
+    public CustomModelData getModelData(EntityNPCInterface npc){
+        return ((IDataDisplay)npc.display).getCustomModelData();
+    }
+    
+    public void addSelectionBlock(int id, int y, String label, String value){
+        this.addLabel(new GuiLabel(id,label, guiLeft - 85, y + 5,0xffffff));
+        addTextField(new GuiTextFieldNop(id,this, guiLeft - 40, y, 200, 20, value));
+        this.addButton(new GuiButtonNop(this,id, guiLeft + 163, y, 80, 20, "mco.template.button.select"));
+    }
+
+    @Override
+    public void buttonEvent(GuiButtonNop button) {
+        if(button.id == 670){
+            close();
+        }
+        if(button.id==1){
+            setSubGui(new GuiStringSelection(this,"Selecting geckolib animation file:",
+                    AnimationFileUtil.getAnimationFileList(), (name)-> getModelData(npc).setAnimFile(name)));
+        }
+        if(button.id==2){
+            setSubGui(new GuiStringSelection(this,"Selecting geckolib idle animation:",
+                    AnimationFileUtil.getAnimationList(getModelData(npc).getAnimFile()),
+                    (name)-> getModelData(npc).setIdleAnim(name)));
+        }
+        if(button.id==3){
+            setSubGui(new GuiStringSelection(this,"Selecting geckolib walk animation:",
+                    AnimationFileUtil.getAnimationList(getModelData(npc).getAnimFile()),
+                    (name)-> getModelData(npc).setWalkAnim(name)));
+        }
+        if(button.id==4){
+            setSubGui(new GuiStringSelection(this,"Selecting geckolib attack animation:",
+                    AnimationFileUtil.getAnimationList(getModelData(npc).getAnimFile()),
+                    (name)-> getModelData(npc).setAnimFile(name)));
+        }
+        if(button.id==5){
+            setSubGui(new GuiStringSelection(this,"Selecting geckolib hurt animation:",
+                    AnimationFileUtil.getAnimationList(getModelData(npc).getAnimFile()),
+                    (name)-> getModelData(npc).setHurtAnim(name)));
+        }
+    }
+
+    public boolean isValidAnimFile(String name){
+        return GeckoLibCache.getInstance().getAnimations().containsKey(new ResourceLocation(name));
+    }
+
+    public boolean isValidAnimation(String name){
+        return AnimationFileUtil.getAnimationList(getModelData(npc).getAnimFile()).contains(name);
+    }
+
+    @Override
+    public void unFocused(GuiTextFieldNop textfield) {
+        if(textfield.id == 1 && isValidAnimFile(textfield.getValue())){
+            if(!textfield.isEmpty())
+                getModelData(npc).setAnimFile(textfield.getValue());
+            else
+                textfield.setValue(getModelData(npc).getAnimFile());
+        }
+        if(textfield.id == 2 && isValidAnimation(textfield.getValue())){
+            if(!textfield.isEmpty())
+                getModelData(npc).setIdleAnim(textfield.getValue());
+            else
+                textfield.setValue(getModelData(npc).getIdleAnim());
+        }
+        if(textfield.id == 3 && isValidAnimation(textfield.getValue())){
+            if(!textfield.isEmpty())
+                getModelData(npc).setWalkAnim(textfield.getValue());
+            else
+                textfield.setValue(getModelData(npc).getWalkAnim());
+        }
+        if(textfield.id == 4 && isValidAnimation(textfield.getValue())){
+            if(!textfield.isEmpty())
+                getModelData(npc).setAttackAnim(textfield.getValue());
+            else
+                textfield.setValue(getModelData(npc).getAttackAnim());
+        }
+        if(textfield.id == 5 && isValidAnimation(textfield.getValue())){
+            if(!textfield.isEmpty())
+                getModelData(npc).setHurtAnim(textfield.getValue());
+            else
+                textfield.setValue(getModelData(npc).getHurtAnim());
+        }
+    }
+}
---
+++ src/main/java/com/goodbird/cnpcgeckoaddon/client/gui/GuiStringSelection.java
@@ -0,0 +1,57 @@
+package com.goodbird.cnpcgeckoaddon.client.gui;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.gui.screen.Screen;
+import noppes.npcs.client.gui.util.GuiNPCInterface;
+import noppes.npcs.shared.client.gui.components.GuiButtonNop;
+import noppes.npcs.shared.client.gui.components.GuiLabel;
+import noppes.npcs.shared.client.gui.components.GuiStringSlotNop;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class GuiStringSelection extends GuiNPCInterface {
+    public GuiStringSlotNop slot;
+    public Consumer<String> action;
+    public Screen parent;
+    public String title;
+    public List<String> options;
+
+    public GuiStringSelection(Screen parent, String title, List<String> options, Consumer<String> action) {
+        drawDefaultBackground = false;
+        this.parent = parent;
+        this.action = action;
+        this.title = title;
+        this.options = options;
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        addLabel(new GuiLabel(0, title, width / 2 - (this.font.width(title) / 2), 20, 0xffffff));
+        options.sort(String.CASE_INSENSITIVE_ORDER);
+        slot = new GuiStringSlotNop(options, this, false);
+        this.children.add(this.slot);
+        this.addButton(new GuiButtonNop(this, 2, width / 2 - 100, height - 44, 98, 20, "gui.back"));
+    }
+
+    @Override
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        this.slot.render(matrixStack, mouseX, mouseY, partialTicks);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    public void doubleClicked() {
+        action.accept(slot.getSelectedString());
+        close();
+    }
+
+    @Override
+    public void buttonEvent(GuiButtonNop guibutton) {
+        int id = guibutton.id;
+        if (id == 2) {
+            close();
+        }
+    }
+}
