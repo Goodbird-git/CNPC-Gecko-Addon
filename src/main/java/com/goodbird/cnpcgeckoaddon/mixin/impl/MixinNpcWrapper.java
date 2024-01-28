a59ef54f161d1aedaac7fa9cce32added37c3030
Goodbird <goodbirdmail@yandex.ru>
2024-01-28 11:29:27 +0300

    Added scripted control over the animations to CNPC API

---
+++ src/main/java/com/goodbird/cnpcgeckoaddon/mixin/impl/MixinNpcWrapper.java
@@ -0,0 +1,27 @@
+package com.goodbird.cnpcgeckoaddon.mixin.impl;
+
+import com.goodbird.cnpcgeckoaddon.network.PacketSyncAnimation;
+import noppes.npcs.api.entity.IPlayer;
+import noppes.npcs.api.wrapper.EntityLivingWrapper;
+import noppes.npcs.api.wrapper.NPCWrapper;
+import noppes.npcs.entity.EntityNPCInterface;
+import noppes.npcs.packets.Packets;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+
+@Mixin(NPCWrapper.class)
+public class MixinNpcWrapper<T extends EntityNPCInterface> extends EntityLivingWrapper<T> {
+    public MixinNpcWrapper() {
+        super(null);
+    }
+
+    @Unique
+    public void syncAnimationsFor(IPlayer player, AnimationBuilder builder) {
+        Packets.send(player.getMCEntity(), new PacketSyncAnimation(entity.getId(),builder));
+    }
+    @Unique
+    public void syncAnimationsForAll(AnimationBuilder builder) {
+        Packets.sendAll(new PacketSyncAnimation(entity.getId(),builder));
+    }
+}
