bfc4e685432b890a98b5442ec16cb06b731824b2
Goodbird <goodbirdmail@yandex.ru>
2024-01-31 16:42:49 +0300

    Fixed main class

--- src/main/java/com/goodbird/cnpcgeckoaddon/CNPCGeckoAddon.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/CNPCGeckoAddon.java
@@ -1,9 +1,7 @@
 package com.goodbird.cnpcgeckoaddon;
 
-import com.goodbird.cnpcgeckoaddon.client.ClientProxy;
 import com.goodbird.cnpcgeckoaddon.network.NetworkWrapper;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -13,14 +11,9 @@ public class CNPCGeckoAddon {
 
     public CNPCGeckoAddon() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         NetworkWrapper.init();
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-        ClientProxy.load();
     }
 }
