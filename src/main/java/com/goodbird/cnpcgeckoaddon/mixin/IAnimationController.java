92c67413694651aa89b3856be6dd7427ccf83124
Goodbird <goodbirdmail@yandex.ru>
2024-02-07 10:40:10 +0300

    Removed mixins from the project because they can not change mods on 1.12.2

--- src/main/java/com/goodbird/cnpcgeckoaddon/mixin/IAnimationController.java
+++
@@ -1,7 +0,0 @@
-package com.goodbird.cnpcgeckoaddon.mixin;
-
-import software.bernie.geckolib3.core.builder.AnimationBuilder;
-
-public interface IAnimationController {
-    AnimationBuilder getCurrentAnimationBuilder();
-}
--- src/main/java/com/goodbird/cnpcgeckoaddon/mixin/IDataDisplay.java
+++
@@ -1,9 +0,0 @@
-package com.goodbird.cnpcgeckoaddon.mixin;
-
-import com.goodbird.cnpcgeckoaddon.data.CustomModelData;
-
-public interface IDataDisplay {
-    CustomModelData getCustomModelData();
-
-    boolean hasCustomModel();
-}
