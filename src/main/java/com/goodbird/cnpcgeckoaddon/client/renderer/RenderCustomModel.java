8d5d36fd32cbe6ccc2114237993056db19c8f469
Goodbird <goodbirdmail@yandex.ru>
2024-05-27 09:25:06 +0300

    Fixed <6 sided cubes not rendering properly

--- src/main/java/com/goodbird/cnpcgeckoaddon/client/renderer/RenderCustomModel.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/client/renderer/RenderCustomModel.java
@@ -4,17 +4,19 @@ import com.goodbird.cnpcgeckoaddon.client.model.ModelCustom;
 import com.goodbird.cnpcgeckoaddon.entity.EntityCustomModel;
 import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
-import software.bernie.geckolib3.geo.render.built.GeoBone;
-import software.bernie.geckolib3.geo.render.built.GeoModel;
+import software.bernie.geckolib3.geo.render.built.*;
 import net.minecraft.util.math.MathHelper;
 
 import javax.annotation.Nullable;
+import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 import java.util.ArrayList;
 
 public class RenderCustomModel extends GeoEntityRendererCompat<EntityCustomModel> {
@@ -95,6 +97,35 @@ public class RenderCustomModel extends GeoEntityRendererCompat<EntityCustomModel
         ResourceLocation entityTexture = getEntityTexture(animatable);
         if(entityTexture!=null) {
             Minecraft.getMinecraft().getTextureManager().bindTexture(entityTexture);
+        }
+    }
+
+    public void renderCube(BufferBuilder builder, GeoCube cube, float red, float green, float blue, float alpha) {
+        MATRIX_STACK.moveToPivot(cube);
+        MATRIX_STACK.rotate(cube);
+        MATRIX_STACK.moveBackFromPivot(cube);
+
+        for (GeoQuad quad : cube.quads) {
+            if (quad == null) continue;
+            Vector3f normal = new Vector3f((float) quad.normal.getX(), (float) quad.normal.getY(), (float) quad.normal.getZ());
+            MATRIX_STACK.getNormalMatrix().transform(normal);
+            if ((cube.size.y == 0.0F || cube.size.z == 0.0F) && normal.getX() < 0.0F) {
+                normal.x *= -1.0F;
+            }
+
+            if ((cube.size.x == 0.0F || cube.size.z == 0.0F) && normal.getY() < 0.0F) {
+                normal.y *= -1.0F;
+            }
+
+            if ((cube.size.x == 0.0F || cube.size.y == 0.0F) && normal.getZ() < 0.0F) {
+                normal.z *= -1.0F;
+            }
+
+            for (GeoVertex vertex : quad.vertices) {
+                Vector4f vector4f = new Vector4f(vertex.position.getX(), vertex.position.getY(), vertex.position.getZ(), 1.0F);
+                MATRIX_STACK.getModelMatrix().transform(vector4f);
+                builder.pos(vector4f.getX(), vector4f.getY(), vector4f.getZ()).tex(vertex.textureU, vertex.textureV).color(red, green, blue, alpha).normal(normal.getX(), normal.getY(), normal.getZ()).endVertex();
+            }
         }
     }
 }
--- src/main/java/com/goodbird/cnpcgeckoaddon/client/renderer/RenderTileCustomModel.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/client/renderer/RenderTileCustomModel.java
@@ -2,10 +2,46 @@ package com.goodbird.cnpcgeckoaddon.client.renderer;
 
 import com.goodbird.cnpcgeckoaddon.client.model.TileModelCustom;
 import com.goodbird.cnpcgeckoaddon.tile.TileEntityCustomModel;
+import net.minecraft.client.renderer.BufferBuilder;
+import software.bernie.geckolib3.geo.render.built.GeoCube;
+import software.bernie.geckolib3.geo.render.built.GeoQuad;
+import software.bernie.geckolib3.geo.render.built.GeoVertex;
 import software.bernie.geckolib3.renderers.geo.GeoBlockRenderer;
+
+import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 
 public class RenderTileCustomModel extends GeoBlockRenderer<TileEntityCustomModel> {
     public RenderTileCustomModel() {
         super(new TileModelCustom());
+    }
+
+    public void renderCube(BufferBuilder builder, GeoCube cube, float red, float green, float blue, float alpha) {
+        MATRIX_STACK.moveToPivot(cube);
+        MATRIX_STACK.rotate(cube);
+        MATRIX_STACK.moveBackFromPivot(cube);
+
+        for (GeoQuad quad : cube.quads) {
+            if (quad == null) continue;
+            Vector3f normal = new Vector3f((float) quad.normal.getX(), (float) quad.normal.getY(), (float) quad.normal.getZ());
+            MATRIX_STACK.getNormalMatrix().transform(normal);
+            if ((cube.size.y == 0.0F || cube.size.z == 0.0F) && normal.getX() < 0.0F) {
+                normal.x *= -1.0F;
+            }
+
+            if ((cube.size.x == 0.0F || cube.size.z == 0.0F) && normal.getY() < 0.0F) {
+                normal.y *= -1.0F;
+            }
+
+            if ((cube.size.x == 0.0F || cube.size.y == 0.0F) && normal.getZ() < 0.0F) {
+                normal.z *= -1.0F;
+            }
+
+            for (GeoVertex vertex : quad.vertices) {
+                Vector4f vector4f = new Vector4f(vertex.position.getX(), vertex.position.getY(), vertex.position.getZ(), 1.0F);
+                MATRIX_STACK.getModelMatrix().transform(vector4f);
+                builder.pos(vector4f.getX(), vector4f.getY(), vector4f.getZ()).tex(vertex.textureU, vertex.textureV).color(red, green, blue, alpha).normal(normal.getX(), normal.getY(), normal.getZ()).endVertex();
+            }
+        }
     }
 }
