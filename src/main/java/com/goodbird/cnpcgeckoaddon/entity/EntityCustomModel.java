273869f76e5f985fe14587c64abc9a5787ffa1d5
Goodbird <goodbirdmail@yandex.ru>
2024-05-31 21:55:47 +0300

    Added a temporary fix for missing attributes

--- src/main/java/com/goodbird/cnpcgeckoaddon/entity/EntityCustomModel.java
+++ src/main/java/com/goodbird/cnpcgeckoaddon/entity/EntityCustomModel.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Pose;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -116,5 +117,13 @@ public class EntityCustomModel extends Animal implements IAnimatable, IAnimation
     @Override
     public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
         return null;
+    }
+
+    public double getAttributeValue(Attribute p_233637_1_) {
+        try {
+            return this.getAttributes().getValue(p_233637_1_);
+        }catch (Exception e){
+            return 1.0;
+        }
     }
 }
